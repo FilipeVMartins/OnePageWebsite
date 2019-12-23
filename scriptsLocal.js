425bc90e2ce75642ebd64d27b3e12ccb105bd209
FilipeVMartins <filipe_2021@hotmail.com>
2019-12-23 00:11:20 -0300

    navbar

--- scriptsLocal.js
+++ scriptsLocal.js
@@ -1,36 +1,38 @@
 $( function(e) {
 
-            function redimensionar(){
-                var alturajanela = $(window).height();
+    function redimensionar(){
+        var alturajanela = $(window).height();
 
-                $("#inicio").css("height", alturajanela);
-                $("#quemsomos").css("height", alturajanela);
-                $("#nossosservicos").css("height", alturajanela);
-                $("#contato").css("height", alturajanela);
-                $("#saibamais").css("height", alturajanela);
+        $("#inicio").css("height", alturajanela);
+        $("#quemsomos").css("height", alturajanela);
+        $("#nossosservicos").css("height", alturajanela);
+        $("#contato").css("height", alturajanela);
+        $("#saibamais").css("height", alturajanela);
 
-            }
+    }
 
-            $("nav div ul li:nth-child(1)").click(function(e){
-                $("html, body").animate({scrollTop:$("#inicio").offset().top}, 1000);
-            });
+    $("nav div ul li:nth-child(1)").click(function(e){
+        $("html, body").animate({scrollTop:$("#inicio").offset().top}, 1000);
+    });
 
-            $("nav div ul li:nth-child(2)").click(function(e){
-                $("html, body").animate({scrollTop:$("#quemsomos").offset().top}, 1000);
-            });
+    $("nav div ul li:nth-child(2)").click(function(e){
+        $("html, body").animate({scrollTop:$("#quemsomos").offset().top}, 1000);
+    });
 
-            $("nav div ul li:nth-child(3)").click(function(e){
-                $("html, body").animate({scrollTop:$("#nossosservicos").offset().top}, 1000);
-            });
+    $("nav div ul li:nth-child(3)").click(function(e){
+        $("html, body").animate({scrollTop:$("#nossosservicos").offset().top}, 1000);
+    });
 
-            $("nav div ul li:nth-child(4)").click(function(e){
-                $("html, body").animate({scrollTop:$("#contato").offset().top}, 1000);
-            });
+    $("nav div ul li:nth-child(4)").click(function(e){
+        $("html, body").animate({scrollTop:$("#contato").offset().top}, 1000);
+    });
 
-            $("nav div ul li:nth-child(5)").click(function(e){
-                $("html, body").animate({scrollTop:$("#saibamais").offset().top}, 1000);
-            });
+    $("nav div ul li:nth-child(5)").click(function(e){
+        $("html, body").animate({scrollTop:$("#saibamais").offset().top}, 1000);
+    });
+    
+    $("main div article").attr("class","pt-md-5");
 
-            $(window).resize(function(e){redimensionar();});
-            redimensionar();
-        });	
+    $(window).resize(function(e){redimensionar();});
+    redimensionar();
+});
